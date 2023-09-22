9f043e32e7bf039689223e596bd1027b1cdd23e8
Arshath222 <arshuabnoor@gmail.com>
2023-09-22 19:02:12 +0530

    my changes

---
+++ src/Sidebar/Price/Price.js
@@ -0,0 +1,11 @@
+import './Price.css';
+function Price() {
+  return <div className='ml'>
+    <h2 className='sidebar-title price-title'>Price</h2>
+    <label className='sidebar-label-container'>
+    <input type='radio' name='test' />
+    <span className='checkmark'></span>All
+  </label>
+  </div>
+}
+export default Price;
